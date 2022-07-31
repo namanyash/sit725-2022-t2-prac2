e332d7a3e39c7e4bb7691259ca0a2aff257df679
namanyash <naman_yash@yahoo.in>
2022-07-31 13:49:00 +1000

    Added the get path /addTwoNumbers

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static(__dirname+'/public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/p/:test', function(req, res) {
-    res.send("Output: " + req.params.test);
+app.get('/addTwoNumbers', function(req, res) {
+    // res.send("Output: " + req.params.test);
+    console.log(req);
+    const num1 =  parseInt(req.query.num1);
+    const num2 =  parseInt(req.query.num2);
+    const answer = num1 + num2 ; 
+    res.send("Sum of numbers " + num1 + " & " + num2 + " is " + answer);
 });
 
-var port = process.env.port || 3001;
+var port = process.env.port || 3000;
 
 app.listen(port, ()=>{
     console.log("App is running on the port: "+ port)
